1eeaeb6b5b16541ced3ef68e0b68e557a4414ddf
SrCh1n0 <facundo.lopez_hd@hotmail.com>
2025-06-03 16:53:21 -0300

    Agrego el modulo para mover tareas

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -5,20 +5,50 @@ int cantTareas = random.Next(3, 6);
 
 List<Tarea> tareasPendientes = new List<Tarea>();
 string descripcion;
-int duracion;
+int duracion, idRealizado;
 for (int i = 0; i < cantTareas; i++)
 {
-
     Console.WriteLine($"Descripcion de la Tarea: ");
     descripcion = Console.ReadLine();
     Console.WriteLine($"Duracion de la tarea: ");
     duracion = Convert.ToInt32(Console.ReadLine());
     tareasPendientes.Add(new Tarea(i + 1, descripcion, duracion));
 }
+
+Console.WriteLine($"Tareas Pendientes: ");
 foreach (Tarea pendiente in tareasPendientes)
 {
-    Console.WriteLine($"Id: " + pendiente.TareaId);
-    Console.WriteLine($"Descripcion: " + pendiente.Descripcion);
-    Console.WriteLine($"Duracion: " + pendiente.Duracion);
+    MostrarTarea(pendiente);
 }
 List<Tarea> tareasRealizadas = new List<Tarea>();
+
+Console.WriteLine($"Id a Realizadas: ");
+idRealizado = Convert.ToInt32(Console.ReadLine());
+
+foreach (Tarea pendiente in tareasPendientes)
+{
+    if (pendiente.TareaId == idRealizado)
+    {
+        tareasRealizadas.Add(pendiente);
+    }
+}
+tareasPendientes.RemoveAt(idRealizado - 1);
+
+Console.WriteLine($"Tareas Pendientes: ");
+foreach (Tarea pendiente in tareasPendientes)
+{
+    MostrarTarea(pendiente);
+}
+
+Console.WriteLine($"Tareas Realizadas: ");
+foreach (Tarea realizado in tareasRealizadas)
+{
+    MostrarTarea(realizado);
+}
+
+static void MostrarTarea(Tarea tarea)
+{
+    Console.WriteLine($"Id: " + tarea.TareaId);
+    Console.WriteLine($"Descripcion: " + tarea.Descripcion);
+    Console.WriteLine($"Duracion: " + tarea.Duracion);
+}
